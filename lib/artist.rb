b7792254dd2b40c4dfe9b5b261b215a9e13b583b
Annie <anniezheng.anniee@gmail.com>
2019-11-23 13:39:47 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require_relative "song"
+class Artist
+    attr_accessor :name
+    def initialize
+        @name = name
+    end
+end #end of Artist class
+
+#drake = Artist.new("Drake")
+
+#binding.pry
+#"end"
