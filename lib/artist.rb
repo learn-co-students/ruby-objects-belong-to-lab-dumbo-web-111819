6ce98095a1382fadefd6f3187ea23fcfcc31849f
Valentine <valentinemaillard1@gmail.com>
2019-11-24 16:33:59 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+# A song should belong to an artist
+
+# Artists should have a name. 
+
+class Artist
+    attr_accessor :name
+
+    def initialize
+        @name = name
+    end
+
+
+end # end of class
---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+# A post should belong to an author.
+
+# Posts should have a title and belong to an author.
+
+class Post
+    attr_accessor :title, :author
+
+    def initialize
+        @title = title
+    end
+
+
+end # end of class
